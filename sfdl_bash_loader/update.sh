9e0826d8aae71b3d7f0f95763221fb6c1e90a982
picardncc <156421422+picardncc@users.noreply.github.com>
2024-01-12 19:44:30 +0100

    Update update.sh

--- sfdl_bash_loader/update.sh
+++ sfdl_bash_loader/update.sh
@@ -40,7 +40,7 @@ if [ $osxcheck == "Darwin" ]; then
 	osxpath="$(realpath "$0")"
 	pwd=$(dirname "${osxpath}")
 else
-	pwd="`dirname \"$0\"`"
+    	pwd=$(cd `dirname $0` && pwd)
 fi
 
 # macht das bild sauber
