1807847bde35324dcbf51b8761bce16faac246e5
picardncc <156421422+picardncc@users.noreply.github.com>
2024-01-12 18:10:43 +0100

    Update update.sh

--- sfdl_bash_loader/update.sh
+++ sfdl_bash_loader/update.sh
@@ -130,6 +130,9 @@ if [ $(($version_local)) -lt $(($version_repo)) ]; then
 	echo "| Download wird gestartet."
 	echo "| Bitte warten ..."
 
+echo "pwd: "$pwd
+exit
+
     wget $url_repodownload -O main.tgz
     tar -xvf main.tgz
     mkdir $pwd/sfdl_bash_loader >/dev/null 2>&1
