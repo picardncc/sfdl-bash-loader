a535323c78c2df8c9dcf9244723efdd9a9343b82
picardncc <156421422+picardncc@users.noreply.github.com>
2024-01-13 08:21:19 +0100

    Update update.sh

--- sfdl_bash_loader/update.sh
+++ sfdl_bash_loader/update.sh
@@ -134,6 +134,7 @@ if [ $(($version_local)) -lt $(($version_repo)) ]; then
 	echo "| Download wird gestartet."
 	echo "| Bitte warten ..."
 
+    rm $pwd/sys/logs/version.txt
 #    rm -rf $pwd >/dev/null 2>&1
 #    mkdir $pwd >/dev/null 2>&1
     mkdir $pwd/tmp >/dev/null 2>&1
