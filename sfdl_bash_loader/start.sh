feb528a280965971c548a79d1511a4c0a92619a5
picardncc <156421422+picardncc@users.noreply.github.com>
2024-01-13 12:18:00 +0100

    Update start.sh

--- sfdl_bash_loader/start.sh
+++ sfdl_bash_loader/start.sh
@@ -79,6 +79,7 @@ if ! [ $sfdl_update = false ]; then
 			#alte update.sh sichern
 			if [ -f "$pwd/update.sh" ]; then
 				mv "$pwd/update.sh" "$pwd/update_old.sh"
+                    		rm "$pwd/sys/logs/version.txt"
 			fi 
 			
 			#hole neues Update script
