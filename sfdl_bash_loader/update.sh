4660d9eca86feb439a23fff487a525eb760b0337
picardncc <156421422+picardncc@users.noreply.github.com>
2024-01-13 08:23:20 +0100

    Update update.sh

--- sfdl_bash_loader/update.sh
+++ sfdl_bash_loader/update.sh
@@ -135,6 +135,7 @@ if [ $(($version_local)) -lt $(($version_repo)) ]; then
 	echo "| Bitte warten ..."
 
     rm $pwd/sys/logs/version.txt
+sleep 5
 #    rm -rf $pwd >/dev/null 2>&1
 #    mkdir $pwd >/dev/null 2>&1
     mkdir $pwd/tmp >/dev/null 2>&1
